b36d196b13aae9a2d7594cc5297cce7c37521875
Lucy Yu <lucyyu540@gmail.com>
2021-02-17 18:42:30 +0900

    sdf

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -6,41 +6,45 @@ public class ReservationSystem {
 		buses = new ArrayList<Bus>();
 		people = new ArrayList<Person>();
 	}
+	/*1*/
 	public void insertBus(String name, int price, int size) {
 		//name, price, size
 		buses.add(new Bus(buses.size()+1, name, price, size));
 	}
+	/*2*/
 	public void insertBus(Scanner in) {
 		//name, price, size
 		System.out.print("[버스이름 티켓값 좌석수]을 입력해주세요 : >> ");
 		String[] arr = read(in).split(" ");
 		buses.add(new Bus(buses.size()+1, arr[0], Integer.parseInt(arr[1]), Integer.parseInt(arr[2])));
 	}
+	/*3*/
 	public void insertPerson(String name, int balance) {
 		//name, balance, 
 		people.add(new Person(people.size()+1, name, balance));
 	}
+	/*4*/
 	public void insertPerson(Scanner in) {
 		//name, balance, 
 		System.out.print("[이름 잔액]을 입력해주세요 : >> ");
 		String[] arr = read(in).split(" ");
 		people.add( new Person(people.size()+1, arr[0], Integer.parseInt(arr[1]) ) );
 	}
+	/*5*/
 	public void makeReservation(Scanner in) {
+		Bus bus; Person person;
 		System.out.print("id 를 입력해주세요 : >> ");
 		int personid = Integer.parseInt(read(in));
-		Bus bus; Person person;
 		person = people.get(personid-1);
 		print(buses);//버스 이름 출력
-		person.Mytickets();//본인 티켓 상황 출력 
-		System.out.print("버스를 선택해주세요 >>");
+		person.MyTickets();//본인 티켓 상황 출력 
+		System.out.print("버스를 선택해주세요 >> ");
 		int busid = Integer.parseInt(read(in));
 		bus = buses.get(busid-1);
 		//만석 
 		if(bus.getCount == bus.getSeats().length) {
-			System.out.print("대기실에 입장하시겠습니까? y/n >>");
+			System.out.print("대기실에 입장하시겠습니까? y/n >> ");
 			if(read(in).equals("y")) bus.addtoqueue(person);
-			else return;
 		}
 		//자리 선택으로 진
 		else {
@@ -78,7 +82,7 @@ public class ReservationSystem {
 	/*HELPER METHODS*/
 	public void print(List<Bus> l) {
 		for (Bus o : l) {
-			System.out.print(o.getId()+ ") "+o.getName()+ " ");
+			System.out.println("[" + o.getId()+ "] "+o.getName());
 		}
 	}
 	public String read(Scanner in) {
