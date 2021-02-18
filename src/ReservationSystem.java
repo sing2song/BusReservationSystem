dbf7ac6e3c949dd47695f8c9b15372d2074cdc1b
ssong <sing2song@naver.com>
2021-02-18 15:02:29 +0900

    update people.txt

--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -8,7 +8,6 @@ public class ReservationSystem {
 	}
 	/*아이디 유효성체크*/
 	public boolean checkId(int id) {
-		System.out.println("id="+id+", people.size():"+people.size());
 		return id<=people.size();
 	}
 	
