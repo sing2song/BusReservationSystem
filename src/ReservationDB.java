8576ad9a747d311f7aec33747f10107c6c903fc2
Lucy Yu <lucyyu540@gmail.com>
2021-02-23 17:12:33 +0900

    mysql

--- src/ReservationDB.java
+++ src/ReservationDB.java
@@ -12,12 +12,37 @@ public class ReservationDB {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("드라이버 로딩 성공");
 			//"jdbc:mysql://localhost:3306?serverTimezone=UTC","root","1234");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampleDB?serverTimezone=UTC","root","ssong");
+			String password = "ssong";
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampleDB?serverTimezone=UTC","root", password);
 			System.out.println("데이터베이스 연결성공");
 			stmt = con.createStatement();
+			//stmt.executeQuery("use busReservationSystem");
 			System.out.println("연결객체 획득 성공");
 		} catch (Exception e) {
 			throw new Exception("데이터베이스 연결 오류");
 		}		
-	}	
+	}
+	/*QUERIES*/
+	public boolean insertPerson() {
+		try {
+			String query = String.format("");
+			executeUpdate(query);
+			return true;
+		}
+		catch(Exception e) {
+			System.out.println("error" + e.getMessage());
+			return false;}
+	}
+	/*BUS*/
+	/*PERSON*/
+	/*TICKET*/
+	/*QUEUE*/
+	private void executeUpdate(String query) throws Exception {
+		try {
+			int rows = stmt.executeUpdate(query); 
+			if (rows<= 0) throw new Exception();
+		}
+		catch(Exception e) {throw new Exception(e.getMessage());}
+	}
+	
 }
