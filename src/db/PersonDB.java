c8585460c1f4ed4efe72817bef8eb2257af98768
ssong <sing2song@naver.com>
2021-02-24 17:26:33 +0900

    persondb

--- src/db/PersonDB.java
+++ src/db/PersonDB.java
@@ -1,36 +1,96 @@
 package db;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
 import model.Person;
 public class PersonDB {
 	public ReservationDB db;
+	public Statement stmt = null;
 	public PersonDB(ReservationDB db) { this.db = db;}
 	/*QUERIES***************************************************/
 	/*CREATE*/
 	public boolean insert(String name, int balance ) {
 		try {
-			String query = String.format("");
+			String query = String.format("insert into person(name,balance) values (%s,%d)",name,balance);
 			db.executeUpdate(query);
 			return true;
 		}
 		catch(Exception e) {
-			System.out.println("error" + e.getMessage());
-			return false;}
+			System.out.println("[person 삽입 실패]" + e.getMessage());
+			return false;
+		}
 	}
 	public Person select(int id) {
-		return null;
+		try {
+			String query 
+			= String.format("select * from person where personid= %d;", id);
+			ResultSet rs = stmt.executeQuery(query);			
+			
+			rs.next();
+			int accid = rs.getInt(1);
+			String name = rs.getString(2);
+			int balance = rs.getInt(3);
+			
+			Person person = new Person(accid,name,balance);
+			
+			return person;
+		}
+		catch(Exception ex) {
+			return null;
+		}
 	}
 	/*READ*/
 	public ArrayList<Person> selectAll() {
-		return null;
+		try {
+			ArrayList<Person> arr = new ArrayList<Person>();
+			String query 
+			= String.format("select * from person ;");
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				int accid = rs.getInt(1);
+				String name = rs.getString(2);
+				int balance = rs.getInt(3);
+				arr.add(new Person(accid, name, balance));
+			}
+			return arr;
+		}
+		catch(Exception ex) {
+			return null;
+		}
 	}
 	/*UPDATE*/
 	public boolean update(int id, int amount) {
-		return false;
+		try {	
+			String query;
+			if( exists(id) == true) 
+			query = String.format("update person set balance= balance+ %d where accid=%d;",
+					amount, id);
+			else
+				query = String.format("update person set balance= balance- %d where accid=%d;",
+						amount, id);
+			
+			db.executeUpdate(query);
+			
+			return true;
+		}
+		catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			return false;
+		}	
 	}
 	/**HELPER***************************************/
 	public boolean exists(int id) {
-		return true;
+		try {
+			String query = String.format("select * from person where personid= %d;", id);
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				return true;
+			}
+		}
+		catch(Exception ex) {
+			return false;
+		}
+		return false;
 	}
 }
