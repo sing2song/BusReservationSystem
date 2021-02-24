156951e86f81ce0bb5fa7b8726958621466eaa50
Lucy Yu <lucyyu540@gmail.com>
2021-02-24 17:40:36 +0900

    password url comment

--- src/db/ReservationDB.java
+++ src/db/ReservationDB.java
@@ -15,6 +15,7 @@ public class ReservationDB {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("드라이버 로딩 성공");
 			String password = "ssong";
+			//String password = "password";
 			//String url = "jdbc:mysql://localhost:3306/busReservationSystem?serverTimezone=UTC";
 			String url = "jdbc:mysql://localhost:3306?serverTimezone=UTC";
 			con = DriverManager.getConnection(url,"root", password);
